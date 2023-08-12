8d992cabadc9cd9d788d8b3060b16b6f63f15f31
Christian Guado <crguado@gmail.com>
2023-08-12 14:12:54 -0500

    Logica para que al hacer click en una card se muestre el componente ProductDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,10 +1,17 @@
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../../Context';
 import { XMarkIcon } from '@heroicons/react/24/solid';
-
 import './styles.css';
 
 function ProducDetail() {
+  const context = useContext(ShoppingCartContext);
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside
+      className={`${
+        context.isProductDetailOpen ? 'flex' : 'hidden'
+      } product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
         <div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,12 +4,24 @@ export const ShoppingCartContext = createContext();
 
 export function ShoppingCartProvider({ children }) {
   const [count, setCount] = useState(0);
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+
+  function openProductDetail() {
+    setIsProductDetailOpen(true);
+  }
+
+  function closeProductDetail() {
+    setIsProductDetailOpen(false);
+  }
 
   return (
     <ShoppingCartContext.Provider
       value={{
         count,
         setCount,
+        openProductDetail,
+        closeProductDetail,
+        isProductDetailOpen,
       }}
     >
       {children}
