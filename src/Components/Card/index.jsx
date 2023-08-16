8592900d78f79bb0e1e4534854d8dba7732e60a0
Christian Guado <crguado@gmail.com>
2023-08-16 18:39:37 -0500

    SideMenu del carrito de compras

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -10,17 +10,19 @@ const Card = data => {
     context.setProductToShow(productDetail)
   }
 
-  const addProductsToCart = productData => {
+  const addProductsToCart = (event, productData) => {
+    event.stopPropagation()
     context.setCount(context.count + 1)
     context.setCartProducts([...context.cartProducts, productData])
+    context.openCheckoutSideMenu()
+    context.closeProductDetail()
     console.log('CART: ', context.cartProducts)
   }
 
   return (
     <div
       className='bg-white cursor-pointer w-56 h-60 rounded-lg'
-      onClick={() => showProduct(data.data)}
-    >
+      onClick={() => showProduct(data.data)}>
       <figure className='relative mb-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>
           {data.data.category.name}
@@ -32,8 +34,7 @@ const Card = data => {
         />
         <div
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-          onClick={() => addProductsToCart(data.data)}
-        >
+          onClick={event => addProductsToCart(event, data.data)}>
           <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
         </div>
       </figure>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -11,6 +11,11 @@ export const ShoppingCartProvider = ({ children }) => {
   const openProductDetail = () => setIsProductDetailOpen(true)
   const closeProductDetail = () => setIsProductDetailOpen(false)
 
+  // Checkout Side Menu · Open/Close
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+
   // Product Detail · Show product
   const [productToShow, setProductToShow] = useState({})
 
@@ -29,8 +34,10 @@ export const ShoppingCartProvider = ({ children }) => {
         setProductToShow,
         cartProducts,
         setCartProducts,
-      }}
-    >
+        isCheckoutSideMenuOpen,
+        openCheckoutSideMenu,
+        closeCheckoutSideMenu,
+      }}>
       {children}
     </ShoppingCartContext.Provider>
   )
