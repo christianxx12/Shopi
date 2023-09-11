e6055fa0e78377c1850cbaf788124d84fc7b0a87
Christian Guado <crguado@gmail.com>
2023-09-10 23:48:03 -0500

    Pagina de MyOrder: orden individual

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -19,6 +19,7 @@ function AppRoutes() {
     { path: '/my-order', element: <MyOrder /> },
     { path: '/my-orders', element: <MyOrders /> },
     { path: '/my-orders/last', element: <MyOrder /> },
+    { path: '/my-orders/:id', element: <MyOrder /> },
     { path: '/signin', element: <SignIn /> },
     { path: '/*', element: <NotFound /> },
   ])
