3997f663bffbd5b6c5ecb983738fd715b760a752
Christian Guado <crguado@gmail.com>
2023-09-14 22:40:22 -0500

    Buscador de productos

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -25,6 +25,19 @@ export const ShoppingCartProvider = ({ children }) => {
   // Shopping Cart · Order
   const [order, setOrder] = useState([])
 
+  // Get products
+  const [items, setItems] = useState(null)
+
+  // Get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+  console.log('searchByTitle: ', searchByTitle)
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
     <ShoppingCartContext.Provider
       value={{
@@ -42,6 +55,10 @@ export const ShoppingCartProvider = ({ children }) => {
         closeCheckoutSideMenu,
         order,
         setOrder,
+        items,
+        setItems,
+        searchByTitle,
+        setSearchByTitle,
       }}>
       {children}
     </ShoppingCartContext.Provider>
