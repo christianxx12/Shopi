e4826894cdf827beec832fad902ee140326cf0d9
Christian Guado <crguado@gmail.com>
2023-09-16 23:50:13 -0500

    Filtrando títulos con JavaScript

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -27,6 +27,7 @@ export const ShoppingCartProvider = ({ children }) => {
 
   // Get products
   const [items, setItems] = useState(null)
+  const [filteredItems, setFilteredItems] = useState(null)
 
   // Get products by title
   const [searchByTitle, setSearchByTitle] = useState(null)
@@ -37,6 +38,17 @@ export const ShoppingCartProvider = ({ children }) => {
       .then(response => response.json())
       .then(data => setItems(data))
   }, [])
+
+  const filteredItemsByTitle = (items, searchByTitle) => {
+    return items?.filter(item =>
+      item.title.toLowerCase().includes(searchByTitle.toLowerCase())
+    )
+  }
+
+  useEffect(() => {
+    if (searchByTitle)
+      setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+  }, [items, searchByTitle])
 
   return (
     <ShoppingCartContext.Provider
@@ -59,6 +71,7 @@ export const ShoppingCartProvider = ({ children }) => {
         setItems,
         searchByTitle,
         setSearchByTitle,
+        filteredItems,
       }}>
       {children}
     </ShoppingCartContext.Provider>
